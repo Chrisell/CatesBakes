b7d2e2f4460a67867b0b35e2c23321936bea711a
Chris Ell <ellbot.chris@gmail.com>
2013-06-11 21:52:46 -0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CatesBakes::Application.config.secret_token = '084e57810716716a196bd68c4cf8b9f1d23f00f4815010258d8a4c8f145b52432df3fae62f1b0518ea6eee2e5708fd442cc107a7843610d4966347bccec0ff97'
